4934086fc326449c40802d9facad9c0a8644b83f
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-15 00:34:11 +0900

    python_lambda homework_age

---
+++ lambda_age.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+people = [
+    ("Blake Howell", "Jamaica", 18, "aw@jul.bw"),
+    ("Peter Bowen", "Burundi", 30, "vinaf@rilkov.il"),
+    ("Winnie Hall", "Palestinian Territories", 22, "moci@pacivhe.net"),
+    ("Alfred Schwartz", "Syria", 29, "ic@tolseuc.pr"),
+    ("Carrie Palmer", "Mauritius", 28, "fenlofi@tor.aq"),
+    ("Rose Tyler", "Martinique", 17, "as@forebjab.et"),
+    ("Katharine Little", "Anguilla", 29, "am@kifez.et"),
+    ("Brent Peterson", "Svalbard & Jan Mayen", 22, "le@wekciga.lr"),
+    ("Lydia Thornton", "Puerto Rico", 19, "lefvoru@itbewuk.at"),
+    ("Richard Newton", "Pitcairn Islands", 17, "da@lasowiwa.su"),
+    ("Eric Townsend", "Svalbard & Jan Mayen", 22, "jijer@cipzo.gp"),
+    ("Trevor Hines", "Dominican Republic", 15, "ev@hivew.tm"),
+    ("Inez Little", "Namibia", 26, "meewi@mirha.ye"),
+    ("Lloyd Aguilar", "Swaziland", 16, "oza@emneme.bb"),
+    ("Erik Lane", "Turkey", 30, "efumazza@va.hn"),
+]
+
+# people.sort(key=lambda x:x[2]) ->people 전체 순차적으로
+people_list = list(filter(lambda tup: tup[2]>20,people))
+# pprint(people_list) -> 20이상만 뽑기
+people_age = people_list
+# pprint(people_age) ->20이상으로 뽑은 애들 순차적으로 하기
+people_age.sort(key=lambda x:x[2])
+pprint(people_age)
