5cff71d62975a5bb3a95be161f362c3dd61813c1
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-16 10:28:38 +0900

    python_while_homework(9.15)
    숫자를 넣으면 곱하기 2
    아니면 해당 문자 프린트

---
+++ while.py
@@ -0,0 +1,23 @@
+def main():
+        count = 0
+        max_count = 5
+        while True:
+                a = input("숫자를 입력해 주세요(종료 - exit) : ")
+                count += 1
+                if count > max_count:
+                        print("5회이상 진행 했습니다!")
+                        break
+                elif a == "exit":
+                        print("게임을 종료하셨습니다.")
+                        break
+                
+                try : 
+                        a = int(a)
+                        count += 1
+                        print(int(a) * 2)  
+                except ValueError:
+                        count -= 1
+                        print(f"입력한 문자는 {a} 입니다!!")   
+               
+
+main()
